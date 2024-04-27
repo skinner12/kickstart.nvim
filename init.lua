e84a3b8e66872ae4737094d56a15db0eb9219828
R. Mazzucchelli <mazzu83@gmail.com>
2024-04-27 11:10:58 +0200

    feat(init): mason dependency for bash code

--- init.lua
+++ init.lua
@@ -582,6 +582,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'taplo', -- Used for toml code
+        'beautysh', -- Used for bash code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
