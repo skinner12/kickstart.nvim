69b5c258268d59a23317e3148d4f7495d4e484a7
R. Mazzucchelli <mazzu83@gmail.com>
2024-05-01 14:42:00 +0200

    refactor(nvim): add colorschemes

--- init.lua
+++ init.lua
@@ -799,6 +799,45 @@ require('lazy').setup({
     -- end,
   },
   {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = 'wave', -- Load "wave" theme when 'background' option is not set
+      -- background = { -- map the value of 'background' option to a theme
+      --   dark = 'wave', -- try "dragon" !
+      --   light = 'lotus',
+      -- },
+    },
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'kanagawa'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
+  {
     'craftzdog/solarized-osaka.nvim',
     lazy = false,
     priority = 1000,
@@ -834,17 +873,16 @@ require('lazy').setup({
       ---@param highlights Highlights
       ---@param colors ColorScheme
       on_highlights = function(highlights, colors) end,
+      -- init = function()
+      --   -- Load the colorscheme here.
+      --   -- Like many other themes, this one has different styles, and you could load
+      --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      --   vim.cmd.colorscheme 'solarized-osaka'
+      --
+      --   -- You can configure highlights by doing something like:
+      --   vim.cmd.hi 'Comment gui=none'
+      -- end,
     },
-
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'solarized-osaka'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
